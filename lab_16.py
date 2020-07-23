fcdae3d00037f297dad1a3d58f77a5177fb9d22e
ofally1989 <timothy.p.ofallon@gmail.com>
2020-07-23 15:53:19 -0400

    Lab 16

---
+++ lab_16.py
@@ -0,0 +1,14 @@
+hostname = "MTG"
+
+if hostname == "MTG":
+    print("The host name was found to be MTG!")
+
+hostname = input("What value should we set for hostname?")
+if hostname == "MTG":
+    print("The hostname was found to be mtg")
+if hostname.lower() == "mtg":
+    print("The hostname was found to be mtg")
+if hostname.lower() == 'mtg':
+    print("The hostname was found to be mtg")
+    print("hostname matches expected config")
+print("exiting the script")
